375bdd9de51853d38375ce4f201aadb4bc0c8e69
Jackson <jackson.roque@protonmail.com>
2026-01-02 20:51:55 -0300

    adiciona scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Um projeto de estudo que recria as principais funcionalidades do TabNews, incluindo publicação de conteúdos, sistema de comentários e interface simplificada.
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,13 @@
 function Home() {
-    return <h1>Jane, eu amo você. Se você me ama, dá uma risadinha! 😘</h1> 
+  return <h1>Jane, eu amo você. Se você me ama, dá uma risadinha! 😘</h1>;
 }
-    
+
+function teste() {
+  console.log("teste");
+}
+
+function teste2() {
+  console.log("indentação errada");
+}
+
 export default Home;
