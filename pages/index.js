5ddb5c2f08bba4af85ac7f1d735eae44f23f32fc
Jackson <jackson.roque@protonmail.com>
2026-01-06 01:25:10 -0300

    remoção de funções de configuração de formatação do código

--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,4 @@ function Home() {
   return <h1>Jane, eu amo você. Se você me ama, dá uma risadinha! 😘</h1>;
 }
 
-function teste() {
-  console.log("teste");
-}
-
-function teste2() {
-  console.log("indentação errada");
-}
-
 export default Home;
